355757690dff9327c3d708b6c8a7913773f1e20d
denise022sanchez-dot <denise022sanchez@gmail.com>
2025-11-29 15:48:55 -0600

    Se declaran las funciones que se usaran, aunque toadavia no se desarrollan

--- Acti5_Calculadora_cientifica/Acti5_Calculadora_cientifica/Acti5_Calculadora_cientifica.cpp
+++ Acti5_Calculadora_cientifica/Acti5_Calculadora_cientifica/Acti5_Calculadora_cientifica.cpp
@@ -1,8 +1,29 @@
-#include <iostream>
-#include <cmath> 
-#include <stdexcept>
+#include <iostream> //Biblioteca estándar para entrada cin y salida cout de datos.
+#include <cmath> //Biblioteca matemática que me permite usar herramientas como pow() para potencia y sqrt() para raíz.
+#include <stdexcept> //Biblioteca de excepciones que permite usar 'runtime_error' para manejar errores sin crashear.
+
 
 using namespace std;
+
+//Se declaran las funciones que se usaran en el programa antes de crearlas 
+
+double sumar(double a, double b); //funciones para sumar dos numeros
+
+
+double sumar(double a, double b, double c); //Declaramos otra función con el MISMO nombre sumar, pero con 3 numeros
+
+double restar(double a, double b);
+double multiplicar(double a, double b);
+
+double dividir(double a, double b);// Esta función promete devolver un double, pero si algo sale mal división por 0, lanzará una excepción.
+
+
+double potencia(double base, int exponente = 2); // Al poner exponente = 2, le decimos al compilador que si el usuario no me da el segundo número, asume que es un 2
+//Esto solo se escribe aquí en el prototipo, no abajo en la implementación.
+
+double raizCuadrada(double numero);
+long long factorial(int n); // Usamos long long porque los factoriales crecen muy rápido y necesitan más memoria
+
 
 int main()
 {
@@ -10,6 +31,6 @@ int main()
 
 
 
-   rerurn 0;
+   return 0;
 }
 
